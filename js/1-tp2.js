a11c0ad4b942d096ec196f05de56caa941de2bee
Joaquin Cortes Carbajo <joaquincortescarbajo@gmail.com>
2022-07-10 18:28:53 -0300

    Termine el 1 ejercicio del Tp2

---
+++ js/1-tp2.js
@@ -0,0 +1,6 @@
+let edad = prompt("Ingresar una edad");
+
+if (edad >= 18) {
+  document.write("La edad es validad para conducir");
+} else edad < 18;
+document.write("La edad ingresada no es validad para conducir");
