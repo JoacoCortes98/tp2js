25bb09b36c03a53d8a68da93cf2445ab99fbe101
Joaquin Cortes Carbajo <joaquincortescarbajo@gmail.com>
2022-07-17 18:51:29 -0300

    termine el ejercicio 12 tp2

---
+++ js/11-tp2.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Ingrese un nombre");
+let numero = parseInt(prompt("Ingrese una edad para ese nombre"));
+let nombre1 = prompt("Ingrese un segundo nombre");
+let numero1 = parseInt(prompt("Ingrese una edad al segundo nombre"));
+let nombre2 = prompt("Ingrese un tercer nombre");
+let numero2 = parseInt(prompt("Ingrese una edad al tercer nombre"));
+
+
